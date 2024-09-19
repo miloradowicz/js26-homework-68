918eb10c4496550b2a4b1e876d1b19775d288ab7
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-09-19 15:32:57 +0600

    AddTaskForm added

--- src/AddTaskForm/AddTaskForm.tsx
+++ src/AddTaskForm/AddTaskForm.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { ChangeEventHandler, FormEventHandler } from 'react';
 
 interface AddTaskFormProps {
   placeholder: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  onSubmit: FormEventHandler<HTMLFormElement>;
 }
 
-const AddTaskForm: React.FC<AddTaskFormProps> = ({ placeholder }) => {
+const AddTaskForm: React.FC<AddTaskFormProps> = ({ placeholder, onChange, onSubmit }) => {
   return (
-    <form>
-      <input type='text' id='' placeholder={placeholder} />
+    <form onSubmit={onSubmit}>
+      <div className='input-group'>
+        <input type='text' placeholder={placeholder} onChange={onChange} className='form-control' />
+        <button type='submit' className='input-group-btn btn btn-success' style={{ width: '5em' }}>
+          Add
+        </button>
+      </div>
     </form>
   );
 };
