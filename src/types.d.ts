087597bf9dd5fb9470408b11838ea0a5bc148fab
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-11 13:06:24 +0600

    types updated

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,10 @@
-export interface Task {
-  get id(): string;
+export interface TaskBody {
   get text(): string;
   get completed(): boolean;
 }
+
+export interface Identifiable {
+  get id(): string;
+}
+
+export interface Task extends TaskBody, Identifiable {}
