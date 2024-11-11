26a126a003aa840502c0445f52b105b1740899e8
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-11 16:59:42 +0600

    TaskForm refactored

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,48 +1,66 @@
-import { TaskBody } from '@/types';
+import { AppDispatch } from '@/app/store';
+import { addTask, getTasks } from '@/containers/Todo/todoSlice';
+import Modal from '@/components/Modal/Modal';
 
-import React, { ChangeEventHandler, FormEventHandler, useState } from 'react';
-
-interface Props {
-  placeholder: string;
-  onSubmit: (task: TaskBody) => void;
-}
+import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 interface FormData {
   text: string;
 }
 
-const TaskForm: React.FC<Props> = ({ placeholder, onSubmit }) => {
+const TaskForm = () => {
   const [data, setData] = useState<FormData>({ text: '' });
+  const [showModal, setShowModal] = useState(false);
+
+  const dispatch = useDispatch<AppDispatch>();
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setData((data) => ({ ...data, [e.target.name]: e.target.value }));
   };
 
-  const handleSubmit: FormEventHandler = (e) => {
+  const handleSubmit: FormEventHandler = async (e) => {
     e.preventDefault();
 
-    onSubmit({ ...data, completed: false });
+    if (data.text) {
+      await dispatch(addTask({ ...data, completed: false }));
+      await dispatch(getTasks());
+    } else {
+      setShowModal(true);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className='input-group'>
-        <input
-          name='text'
-          type='text'
-          placeholder={placeholder}
-          onChange={handleChange}
-          className='form-control'
-        />
-        <button
-          type='submit'
-          className='input-group-btn btn btn-success'
-          style={{ width: '5em' }}
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className='input-group'>
+          <input
+            name='text'
+            type='text'
+            placeholder='Add task'
+            onChange={handleChange}
+            className='form-control'
+          />
+          <button
+            type='submit'
+            className='input-group-btn btn btn-success'
+            style={{ width: '5em' }}
+          >
+            Add
+          </button>
+        </div>
+      </form>
+      {!showModal ? null : (
+        <Modal
+          title='Enter the task name'
+          onClose={() => {
+            setShowModal(false);
+          }}
         >
-          Add
-        </button>
-      </div>
-    </form>
+          Task name cannot be empty.
+        </Modal>
+      )}
+    </>
   );
 };
 
