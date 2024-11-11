691a1b4bf59c36e280a2bb0e3c992acf450452b1
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-11 17:59:14 +0600

    modalSlice attached

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
+import { modalReducer } from '@/components/Modal/modalSlice';
 import { todoReducer } from '@/containers/Todo/todoSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {
+    modal: modalReducer,
     todo: todoReducer,
   },
 });
